04a203597369af260dca39cf951644f6918d09b1
dsc712 <devyendushekhar712@gmail.com>
2018-06-28 15:08:19 +0530

    add todolist starter code

---
+++ js basics/js part-3/todolist/script.js
@@ -0,0 +1,33 @@
+window.onload = function(){
+    let button = document.getElementById("btn") ;
+    let input = document.getElementById("inp") ;
+    let output = document.getElementById("result") ;
+    let value = "" ;
+    let todoList = [] ;
+    button.onclick = function () {
+        value += `<li>${input.value}</li>` ;
+        todoList.push(value) ;
+    }
+
+    function display(){
+        let list = "" ;
+        for(let todo = 0 ; todo < todoList.length ; todo++ ){
+            list += todoList[todo] ;
+        }
+
+        output.innerHTML = list ;
+    }
+
+}
+
+// push()  - it adds the element at end
+// slice(begin , end )  -  it deletes the element and return a new array  [begin , end)
+//splice(begin , end , newitem1 , newitem2 .... )  - it can add as well as delete element and it
+// performs operations on original array instead of returning new array with modification
+
+
+//tasks
+
+// add a function update
+// add a function delete
+// add markAsDone ( optional )
